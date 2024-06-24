a84f169fe9ac56bd3f6dc824e9ab537f49650e6d
Yamayou-Omiya <hellloworllld0101@gmail.com>
2024-06-24 18:40:37 +0900

    フォルダもろもろ追加

---
+++ ABC360/D/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+
+        string[] input = Console.ReadLine().Split(' ');
+        int N = int.Parse(input[0]);
+        int K = int.Parse(input[1]);
+        Console.Write();
+
+
+    } 
+
+}
