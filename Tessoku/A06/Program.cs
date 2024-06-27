95a6d1103edef8b3c9c28052685f66a37c64daf1
Yamayou-Omiya <hellloworllld0101@gmail.com>
2024-06-27 13:17:29 +0900

    update 33 files and delete 7 files

--- Tessoku/A06/Program.cs
+++ Tessoku/A06/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        int n = int.Parse(input[0]);
+        int q = int.Parse(input[1]);
+        
+        int[] a = new int[n+1];
+        input = Console.ReadLine().Split(' ');
+        for (int i = 1; i <= n; i++) a[i] = int.Parse(input[i-1]);
+
+        int[] sum = new int[n+1];
+        sum[0] = 0;
+        for (int i = 1; i <= n; i++) sum[i] = sum[i - 1] + a[i];
+
+        for (int i = 0; i < q; i++)
+        {
+            input = Console.ReadLine().Split(' ');
+            int l = int.Parse(input[0]);
+            int r = int.Parse(input[1]);
+
+            Console.WriteLine(sum[r] - sum[l-1]);
+        }
+    }
+}
