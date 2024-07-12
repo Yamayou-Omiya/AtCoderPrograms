9561d612b67f28c1c9f4ab1dcdca2d97582ef3b9
Yamayou-Omiya <hellloworllld0101@gmail.com>
2024-07-12 13:01:14 +0900

    update 5 files

--- Tessoku/A16/Program.cs
+++ Tessoku/A16/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] a = new int[100010];
+        int[] b = new int[100010];
+
+        string[] input = Console.ReadLine().Split(' ');
+        for (int i = 2; i <= n; i++) a[i] = int.Parse(input[i - 2]);
+
+        input = Console.ReadLine().Split(' ');
+        for (int i = 3; i <= n; i++) b[i] = int.Parse(input[i - 3]);
+
+        
+
+    }
+}
