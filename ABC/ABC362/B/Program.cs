2a2cd5b45fe24cc9c86d9cfd41a4aac273b6bc0f
Yamayou-Omiya <hellloworllld0101@gmail.com>
2024-07-15 00:38:45 +0900

    update Program.cs

--- ABC/ABC362/B/Program.cs
+++ ABC/ABC362/B/Program.cs
@@ -12,10 +12,10 @@ class Program
             (point[i,0], point[i,1]) = (int.Parse(input[0]), int.Parse(input[1]));
         }
 
-        double a,b,c;
-        a = Math.Pow(point[0,0] - point[1,0], 2) + Math.Pow(point[0,1] - point[1,1], 2);
-        b = Math.Pow(point[1,0] - point[2,0], 2) + Math.Pow(point[1,1] - point[2,1], 2);
-        c = Math.Pow(point[2,0] - point[0,0], 2) + Math.Pow(point[2,1] - point[0,1], 2);
+        int a,b,c;
+        a = (int)(Math.Pow(point[0,0] - point[1,0], 2) + Math.Pow(point[0,1] - point[1,1], 2));
+        b = (int)(Math.Pow(point[1,0] - point[2,0], 2) + Math.Pow(point[1,1] - point[2,1], 2));
+        c = (int)(Math.Pow(point[2,0] - point[0,0], 2) + Math.Pow(point[2,1] - point[0,1], 2));
 
         if(a+b==c || b+c==a || c+a==b) Console.WriteLine("Yes");
         else Console.WriteLine("No");
